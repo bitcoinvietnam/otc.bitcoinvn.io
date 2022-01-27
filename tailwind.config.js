71e2f442870b4f7e7e5a80a3a901cb94619bee9f
rowenslee <37297314+rowenslee@users.noreply.github.com>
2022-01-28 06:14:15 +0700

    Fix images path

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,11 +65,11 @@ module.exports = {
         hero: '4.5rem',
       },
       backgroundImage: {
-        'hero-banner': 'url("/assets/images/hero-banner.jpg")',
-        'leader-avt': 'url("/assets/images/leader-avt-black.jpg")',
+        'hero-banner': 'url("~/assets/images/hero-banner.jpg")',
+        'leader-avt': 'url("~/assets/images/leader-avt-black.jpg")',
         'supported-currencies':
-          'url("/assets/images/supported-currencies.jpg")',
-        'who-we-serve': 'url("/assets/images/who-we-serve.jpg")',
+          'url("~/assets/images/supported-currencies.jpg")',
+        'who-we-serve': 'url("~/assets/images/who-we-serve.jpg")',
       },
     },
   },
