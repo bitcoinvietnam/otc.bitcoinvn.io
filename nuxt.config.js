656f51edb88ba012644b14820b988fe6ba4b28c9
rowenslee <rowenslee@TL-ACER.(none)>
2023-06-23 13:28:49 +0700

    Add gtag script

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,5 +53,17 @@ export default {
         },
       },
     },
-  }
+  },
+
+  script: [
+    { src: 'https://www.googletagmanager.com/gtag/js?id=G-8PYNETYLPH', async: true },
+    {
+      innerHTML: `window.dataLayer = window.dataLayer || [];
+                  function gtag(){ dataLayer.push(arguments); };
+                  gtag('js', new Date());
+                  gtag('config', 'G-8PYNETYLPH');`,
+      type: 'text/javascript',
+      charset: 'utf-8'
+    }
+  ]
 }
