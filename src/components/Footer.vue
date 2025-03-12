1100d6e687d969027eafcbd37ec77870a732d804
TL <rowens.lee@gmail.com>
2025-03-12 11:17:07 +0700

    fix: add wrapping for footer links on small screen

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,36 @@
 <template>
-  <footer class="text-center text-light py-4"
+  <footer class="text-center  py-md-4"
     style="background: linear-gradient(to bottom, var(--third-color), var(--secondary-color))">
     <h5>
       <a href="#_" class="text-decoration-none" style="color: var(--primary-color);">Contact Us</a>
     </h5>
-    <nav class="mb-2">
-      <a href="https://bitcoinvn.io" target="_blank" class="text-light mx-2 text-decoration-none">Exchange</a>
-      <a href="https://bitcoinvn.io/news" target="_blank" class="text-light mx-2 text-decoration-none">News</a>
-      <a href="https://bitcoinvn.io/shop" target="_blank" class="text-light mx-2 text-decoration-none">Shop</a>
-      <a href="https://otc.bitcoinvn.io" target="_blank" class="text-light mx-2 text-decoration-none">OTC</a>
-      <a href="https://bitcoinvn.io/btm" target="_blank" class="text-light mx-2 text-decoration-none">BTM</a>
-      <a href="https://newsletter.bitcoinvn.io" target="_blank"
-        class="text-light mx-2 text-decoration-none">Newsletter</a>
+    <nav class="mb-2 d-flex flex-wrap justify-content-center text-center">
+      <a href="https://bitcoinvn.io" target="_blank">Exchange</a>
+      <a href="https://bitcoinvn.io/news" target="_blank">News</a>
+      <a href="https://bitcoinvn.io/shop" target="_blank">Shop</a>
+      <a href="https://otc.bitcoinvn.io" target="_blank">OTC</a>
+      <a href="https://bitcoinvn.io/btm" target="_blank">BTM</a>
+      <a href="https://newsletter.bitcoinvn.io" target="_blank">Newsletter</a>
     </nav>
     <p class="m-0">© 2014 - 2025 BitcoinVN</p>
   </footer>
 </template>
+
+<style scoped>
+nav a,
+p {
+  color: #6c757d;
+  text-decoration: none;
+  margin: 0 0.7rem;
+}
+
+@media (max-width: 768px) {
+
+  nav a,
+  p {
+    color: #6c757d;
+    text-decoration: none;
+    margin: 0 0.4rem;
+  }
+}
+</style>
