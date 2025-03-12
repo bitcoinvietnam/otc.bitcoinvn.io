0ebc8e854900bdc02848db9bf59bb490e64d56c7
TL <rowens.lee@gmail.com>
2025-03-12 13:26:03 +0700

    feat: add homepage link to header logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,8 @@
   <header>
     <nav class="navbar">
       <div class="container-fluid">
-        <a
-          class="navbar-brand fs-md-4 fs-5 ps-2 fw-semibold"
-          href="#"
-          style="color: var(--text-color)"
-        >
-          <img
-            src="@/assets/icon.svg"
-            alt="Logo"
-            width="auto"
-            height="36"
-            class="d-inline-block align-top"
-          />
+        <a class="navbar-brand fs-md-4 fs-5 ps-2 fw-semibold" href="#_" style="color: var(--text-color)">
+          <img src="@/assets/icon.svg" alt="Logo" width="auto" height="36" class="d-inline-block align-top" />
           BitcoinVN <span class="fw-bolder">OTC</span>
         </a>
       </div>
